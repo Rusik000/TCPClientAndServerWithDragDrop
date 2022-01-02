de4da9445809b280a96af29f51c1c8f33d7116b8
Must_nu73@itstep.edu.az <Must_nu73@itstep.edu.az>
2022-01-02 16:27:51 +0400

    Add project files.

---
+++ TCPClientAndServerWithDragDrop/ViewModels/MainViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TCPClientAndServerWithDragDrop.Command;
+
+namespace TCPClientAndServerWithDragDrop.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainWindow MainView { get; set; }
+
+        public RelayCommand ConnectCommand { get; set; }
+        public RelayCommand DisconnectCommand { get; set; }
+
+        public RelayCommand DropCommand { get; set; }
+
+        public string[] Text { get; set; }
+
+
+        TcpClient client = new TcpClient();
+
+        public MainViewModel()
+        {
+
+            Text = new string[] { };
+            DropCommand = new RelayCommand((sender) =>
+            {
+                MainView.DragDropListBx.Drop += DragDrop;
+            });
+
+            ConnectCommand = new RelayCommand((sender) =>
+            {
+                Send();
+
+            }, (pred) =>
+            {
+                if (MainView.ClientName.Text != string.Empty)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            });
+
+            DisconnectCommand = new RelayCommand((sender) =>
+            {
+                client.Dispose();
+                MessageBox.Show("Disconnect . . .");
+            });
+        }
+
+        private void DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files)
+            {
+                MainView.DragDropListBx.Items.Add(file);
+            }
+
+            Text = files;
+            e.Effects = DragDropEffects.All;
+        }
+
+        private void Send()
+        {
+            var ipadress = IPAddress.Parse("192.168.1.109");
+            var port = 27001;
+            var ep = new IPEndPoint(ipadress, port);
+
+            try
+            {
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    foreach (var t in Text)
+                    {
+                        var clientName = MainView.ClientName.Text;
+                        var text = t + " " + clientName;
+                        var stream = client.GetStream();
+                        var bw = new BinaryWriter(stream);
+                        bw.Write(text);
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ TCPServer/ViewModels/MainViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using TCPServer.Command;
+
+namespace TCPServer.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainWindow MainView { get; set; }
+        public RelayCommand PlayCommand { get; set; }
+        public RelayCommand StartServerCommand { get; set; }
+
+
+        private string _clientName;
+
+        public string ClientName
+        {
+            get { return _clientName; }
+            set { _clientName = value; OnPropertyChanged(); }
+        }
+
+        public string SendedMessage { get; set; }
+
+        static TcpListener listener = null;
+
+        static BinaryReader br = null;
+
+
+
+        public MainViewModel()
+        {
+            SendedMessage = "";
+
+            StartServerCommand = new RelayCommand((sender) =>
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Receive();
+
+                });
+                thread.Start();
+            });
+
+            PlayCommand = new RelayCommand((sender) =>
+            {
+                Thread thread = new Thread(() =>
+                {
+                    ShowFile();
+                });
+                thread.Start();
+            });
+        }
+
+        private void ShowFile()
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                Process.Start($@"{SendedMessage}");
+            });
+        }
+
+        private void Receive()
+        {
+            try
+            {
+                var ip = IPAddress.Parse("192.168.1.109");
+
+                var port = 27001;
+
+                var ep = new IPEndPoint(ip, port);
+
+                listener = new TcpListener(ep);
+
+                listener.Start();
+
+                while (true)
+                {
+                    var client = listener.AcceptTcpClient();
+                    MessageBox.Show("Connected");
+
+                    var stream = client.GetStream();
+                    br = new BinaryReader(stream);
+
+                    var msg = br.ReadString();
+                    string[] subMessage = msg.Split(' ');
+                    SendedMessage = subMessage[0];
+                    ClientName = "From" + "-" + subMessage[1];
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
